40fdaa114fa2de0086c3d8ad2330241597294dff
Your Name <you@example.com>
2023-11-10 13:33:35 +0100

    gondoljatek edit

--- pythonteszt/gondoljatek.py
+++ pythonteszt/gondoljatek.py
@@ -1,9 +1,27 @@
 import random
-
+# random.seed(0)
 gondolt = random.randint(1,100)
 
-tipp = 0
-while tipp < 1 or tipp > 100:
-    tipp = int(input("Kérek egy 1 és 100 közé eső számot: "))
-    
-print(tipp)
+talalt = False
+tippSzam = 0
+while not talalt:
+    egesz = 0
+    while egesz < 1 or egesz > 100:
+        egesz = int(input("Kérek egy 1 és 100 közé eső számot: "))
+    tippSzam += 1
+    '''
+    if gondolt < egesz:
+        print("Nagyobb szám")
+    if gondolt > egesz:
+        print("Kisebb szám")
+    if gondolt == egesz:
+        print("Talált")
+        talalt = True
+    '''
+    if gondolt < egesz:
+        print("Tipped nagyobb szám")
+    elif gondolt > egesz:
+        print("Tipped kisebb szám")
+    else:
+        print(tippSzam,"lépésben talált")
+        talalt = True
